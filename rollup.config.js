ed2b198ed072e2c796f4ef6bd36c0ba2be01dd8a
Shane Holloway <shane.holloway@ieee.org>
2018-02-26 12:30:07 -0700

    Updated rollup infrastructure to 0.56.3

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,29 @@
-import rpi_babel from 'rollup-plugin-babel'
+import rpi_jsy from 'rollup-plugin-jsy-babel'
 import pkg from './package.json'
 
 const sourcemap = 'inline'
 const external = ['fs', 'util', 'path', 'child_process', 'chokidar', 'rollup', 'rollup-plugin-babel']
-const plugins = [jsy_plugin()]
+const plugins = [rpi_jsy()]
 
 export default [
   { input: 'code/index.jsy',
     output: [
-      {file: pkg.module, format: 'es'},
-      {file: pkg.main, format: 'cjs'}],
-    sourcemap, external, plugins },
+      {file: pkg.module, format: 'es', sourcemap},
+      {file: pkg.main, format: 'cjs', sourcemap}],
+    external, plugins },
 
   { input: 'code/live-server.jsy',
     output: [
-      {file: './dist/live-server.mjs', format: 'es'},
-      {file: './dist/live-server.js', format: 'cjs'}],
-    sourcemap, external, plugins },
+      {file: 'esm/live-server.js', format: 'es', sourcemap},
+      {file: 'cjs/live-server.js', format: 'cjs', sourcemap}],
+    external, plugins },
 
   { input: 'code/live-loader.jsy',
-    output: [{file: 'dist/live-loader.mjs', format: 'es'}],
-    sourcemap, external: [], plugins },
+    output: [{file: 'esm/live-loader.js', format: 'es', sourcemap}],
+    external: [], plugins },
 
   { input: 'code/react-live-loader.jsy',
-    output: [{file: 'dist/react-live-loader.mjs', format: 'es'}],
-    sourcemap, external: ['react-error-overlay'], plugins },
-]
+    output: [{file: 'esm/react-live-loader.js', format: 'es', sourcemap}],
+    external: ['react-error-overlay'], plugins },
 
-
-function jsy_plugin() {
-  const jsy_preset = [ 'jsy/lean', { no_stage_3: true, modules: false } ]
-  return rpi_babel({
-    exclude: 'node_modules/**',
-    presets: [ jsy_preset ],
-    plugins: [],
-    babelrc: false }) }
-
+].filter(e=>e)
